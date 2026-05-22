78b2fec869e4d5a1949a049374d00376193eb73e
martin.kokonyan <martin.kokonyan@paysafe.com>
2026-05-22 10:41:22 +0300

    Update index.ts, tsconfig.json

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+
+const port: number = 3000;
+const app: Elysia = new Elysia().listen(port);
+
+
+const hostname: string | undefined = app.server?.hostname;
+const serverPort: number | undefined = app.server?.port;
+
+console.log(`Server running at ${hostname}:${serverPort}`);
